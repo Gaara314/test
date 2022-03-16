4f82db700267e9e3b79359a6d992ee11585d21f7
gaara314 <pierre.bourasseau@icloud.com>
2022-03-16 16:01:37 +0100

    initialisation

--- testss.R
+++ testss.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 kg
 #fnfjf
-git config --global user.email "pierre.bourasseau@icloud.com"
-git config --global user.name "gaara314"
+#git config --global user.email "pierre.bourasseau@icloud.com"
+#git config --global user.name "gaara314"
+
+
+library(GNOM)
